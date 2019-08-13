bd2784889ef8e27e3db6a0ac9476a6e6df393f10
Michael McClimon <michael@mcclimon.org>
2019-08-13 06:45:26 -0400

    Convert mtoFn.pl to App::Cmd

---
+++ lib/App/MTO/Command/footnotes.pm
@@ -0,0 +1,34 @@
+package App::MTO::Command::footnotes;
+# ABSTRACT: munge footnote text
+
+use App::MTO -command;
+use v5.24;
+use warnings;
+use experimental 'signatures';
+
+sub usage_desc { "%c fn FILE" }
+
+sub command_names { return ('fn', 'footnotes') }
+
+sub opt_spec { }
+
+sub validate_args ($self, $opt, $args) {
+  print $self->usage_error('no file given') unless @$args == 1;
+}
+
+sub execute ($self, $opt, $args) {
+  require Path::Tiny;
+  my $fn = shift @$args;
+  my $path = Path::Tiny::path($fn);
+
+  my $fn_num = 1;
+  for my $line ($path->lines({ chomp => 1 })) {
+    next if /^\s*$/;
+    printf(qq{  \$fn%d =  array("%d", "%s");\n},
+      $fn_num, $fn_num, $line
+    );
+    $fn_num++;
+  }
+}
+
+1;
